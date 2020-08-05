b31f55791fe6ee5388802674a0c02db5821b7d1e
nosferatum2 <nosferatum3@i.ua>
2020-08-05 20:17:56 +0300

    Gets all URL of the catalog (all categories of second and third lvl). Creates mllucksqlite and inrest URLs of the first items of all categories. sqlite do many-to-many table.

--- mlluckspider.py
+++ mlluckspider.py
@@ -1,8 +1,5 @@
 import sqlite3
-import urllib.error
 import ssl
-from urllib.parse import urljoin
-from urllib.parse import urlparse
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 from datetime import datetime
@@ -21,16 +18,17 @@ cur.execute('''CREATE TABLE IF NOT EXISTS Pages
     (id INTEGER PRIMARY KEY, url TEXT UNIQUE, html TEXT,
      error INTEGER, old_date REAL, new_date REAL)''')
 
-# cur.execute('''CREATE TABLE IF NOT EXISTS Links
-#     (from_id INTEGER, to_id INTEGER, UNIQUE(from_id, to_id))''')
+cur.execute('''CREATE TABLE IF NOT EXISTS Links
+    (from_id INTEGER, to_id INTEGER, UNIQUE(from_id, to_id))''')
 
 cur.execute('''CREATE TABLE IF NOT EXISTS Webs (url TEXT UNIQUE)''')
 
 # Check to see if we are already in progress...
 cur.execute('SELECT id,url FROM Pages WHERE html is NULL and error is NULL ORDER BY RANDOM() LIMIT 1')
 row = cur.fetchone()
+
 if row is not None:
-    print("Restarting existing crawl.  Remove spider.sqlite to start a fresh crawl.")
+    print("Restarting existing crawl.  Remove mlluck.sqlite to start a fresh crawl.")
 else:
     starturl = input('Enter web url or enter: ')
     if (len(starturl) < 1):
@@ -64,16 +62,8 @@ except KeyboardInterrupt:
     print('Program interrupted by user...')
 
 
-# Find url's of the main catalog
-menu_fst_categ = soup.find_all('li', {'class': 'sub_category onhoverclass'})
-count = 0
-for i in menu_fst_categ:
-    item = i.find('a').get('href')
-    if item[0:2] == '//':
-        item = 'https:' + item
-    count += 1
-    print('Main catalog', item)
-print('COUNT:', count)
+
+new_url = set()
 
 # Find second categories of the catalog
 menu_sec_categ = soup.find_all('li', {'class': 'sub_category active_menu_second'})
@@ -82,8 +72,12 @@ for i in menu_sec_categ:
     item = i.find('a').get('href')
     if item[0:2] == '//':
         item = 'https:' + item
+    cur.execute('INSERT OR IGNORE INTO Pages (url, html, new_date) VALUES ( ?, NULL, ? )', (item, DATE))
+    conn.commit()
+    
+    new_url.add(item)
     count += 1
-    print('Second category', item)
+    # print('Second category', item)
 print('COUNT:', count)
 
 
@@ -99,11 +93,101 @@ for i in menu_third_categ:
         title = row.find('span').contents[0]
         if item[0:2] == '//':
             item = 'https:' + item
+        cur.execute('INSERT OR IGNORE INTO Pages (url, html, new_date) VALUES ( ?, NULL, ? )', (item, DATE))
+        conn.commit()
+        new_url.add(item)
         count += 1
-        print(item, title)
+        # print(item, title)
 print('COUNT:', count)
 
+# count = 0
+# for i in new_url:
+#     cur.execute('INSERT OR IGNORE INTO Pages (url, html, new_date) VALUES ( ?, NULL, ? )', (i, DATE))
+#     if count % 100 == 0:
+#         conn.commit()
+#     count += 1
+    # print('SET ITEM', i)
+# print(new_url, len(new_url), sep='\n')
+
+
+setcount = len(new_url)-504
+print('setcount', setcount)
+while True:
+    if (setcount <= 1):
+        break
+    setcount -= 1
+    print("==============While")
+
+    cur.execute('SELECT id,url FROM Pages WHERE html is NULL and error is NULL ORDER BY RANDOM() LIMIT 1')
+    try:
+        row = cur.fetchone()
+        print(row)
+        fromid = row[0]
+        url = row[1]
+    except:
+        print('No unretrieved HTML pages found')
+        many = 0
+        break
+
+    # If we are retrieving this page, there should be no links from it
+    # cur.execute('DELETE from Links WHERE from_id=?', (fromid,))
+
+    try:
+        document = urlopen(url, context=ctx)
+        html = document.read()
+
+        if document.getcode() != 200:
+            print("Error on page: ", document.getcode())
+        if 'text/html' != document.info().get_content_type():
+            print("Ignore non text/html page")
+
+        soup = BeautifulSoup(html, 'html.parser')
+    except KeyboardInterrupt:
+        print('')
+        print('Program interrupted by user...')
+
+
+
+
+    # print(soup.prettify())
+    items_url = soup.find_all('div', {'class': 'ccitem2'})
+    count = 0
+    for item in items_url:
+        itemu = item.a.get('href')
+        print(itemu)
+        cur.execute('INSERT OR IGNORE INTO Pages (url, html, new_date) VALUES ( ?, NULL, ? )', (itemu, DATE))
+        conn.commit()
+
+        cur.execute('SELECT id FROM Pages WHERE url=? LIMIT 1', (itemu,))
+        try:
+            row = cur.fetchone()
+            toid = row[0]
+        except:
+            print('Could not retrieve id')
+            continue
+        # print fromid, toid
+        cur.execute('INSERT OR IGNORE INTO Links (from_id, to_id) VALUES ( ?, ? )', (fromid, toid))
+        conn.commit()
+        count += 1
+        print(fromid, toid)
+    print("==============While end", setcount,'/', len(new_url))
+
+conn.commit()
+
+
+
+
 cur.close()
+
+
+
+
+
+
+
+
+
+
 
 
 
